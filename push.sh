0f9d885c6997f1d002f163b81bc2d3fbe20a3d6e
insomnia <insomnia6974@gmail.com>
2022-04-05 18:07:08 +0800

    update sh

--- push.sh
+++ push.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+export http_proxy=http://127.0.0.1:7890
+export https_proxy=http://127.0.0.1:7890
+
 git status
-read -p "[ ] Add all? (yes/no) \n> " ANS
+read -p "[ ] Add all? (yes/no) > " ANS
 case "$ANS" in
     [yY] | [yY][eE][sS])
         echo "[+] Got it! Adding..."
 		git add --all
-		read -p "[*] commits:\n> " COM
+		read -p "[*] commits: > " COM
 		git commit -m $COM
 		git status
-		read -p "[ ] final check: (yes/no) \n> " FINAL
+		read -p "[#] final check: (yes/no) > " FINAL
 		
 		case "$FINAL" in
 			[yY] | [yY][eE][sS])
